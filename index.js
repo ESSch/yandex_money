bbc8d6caca204f63bd46acfb607b98332e75ac5d
ESSch <ESSch@yandex.ru>
2017-09-24 19:30:03 +0300

    get

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require('express');
+const express 	= require('express');
+const fs 		= require('fs');
+const { URL }	= require('url');
+const fileUrl 	= '/home/essch/test_yandexapp/node-school-wallet-app/source/cards.json';
 const app = express();
 
 app.use(express.static('public'));
@@ -26,6 +29,23 @@ app.get('/transfer', (req, res) => {
 		amount,
 		from,
 		to
+	});
+});
+
+app.get('/cards', (req, res) => {
+	fs.readFile(fileUrl, 'utf8', function (err, cont) {
+		res.end(cont);
+	});
+});
+
+app.get('/cards/add', (req, res) => {
+	fs.readFile(fileUrl, 'utf8', function (err, cont) {
+		let data = JSON.parse(cont);
+		data.apply({
+			cardNumber: 2,
+			balance: 1
+		});
+		res.end(JSON.stringify(data));
 	});
 });
 
